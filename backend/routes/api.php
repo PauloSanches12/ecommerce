6bf23c9ebbbc9c1ebfd2aea13ba2b8a5eec698a1
Paulo Sergio <paulo_sanches2012@hotmail.com>
2025-03-03 21:46:50 -0300

    auterando rotas api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,19 +6,14 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// Rotas protegidas (autenticadas)
-Route::resource('products', ProductController::class)->only(['store', 'update', 'destroy'])->middleware('auth:sanctum');
-Route::resource('categories', CategoryController::class)->only(['store', 'update', 'destroy'])->middleware('auth:sanctum');
-
-// Rotas públicas (não autenticadas)
-Route::resource('products', ProductController::class)->only(['index', 'show']);
-Route::resource('categories', CategoryController::class)->only(['index', 'show']);
-
-// Rotas de autenticação
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('products', ProductController::class);
+    Route::resource('categories', CategoryController::class);
+});
